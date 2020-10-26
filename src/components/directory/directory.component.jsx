27a2294153f2a1a5bd5c5d1994b7fa5cc973b743
David <David@DESKTOP-2HA6BHP>
2020-10-26 17:51:22 +1300

    React App- Add haeder,collection preview and items

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,11 +12,13 @@ class Directory extends React.Component {
                     title: 'hats',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                     id: 1,
+                    linkUrl: 'hats',
                 },
                 {
                     title: 'jackets',
                     imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                     id: 2,
+                    linkUrl: '',
                 },
                 {
                     title: 'sneakers',
@@ -28,12 +30,14 @@ class Directory extends React.Component {
                     imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                     id: 4,
                     size: 'large',
+                    linkUrl: '',
                 },
                 {
                     title: 'men',
                     imageUrl: 'https://i.ibb.co/R70vBrQ/mens.png',
                     id: 5,
                     size: 'large',
+                    linkUrl: '',
                 },
             ]
         }
@@ -42,8 +46,8 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({title, imageUrl, size, id}) =>(
-                        <MenuItem key={id} title={title.toUpperCase()} imageUrl={imageUrl} size={size}></MenuItem>
+                    this.state.sections.map(({id, ...otherSectionProps}) =>(
+                        <MenuItem key={id} {...otherSectionProps}></MenuItem>
                     ))
                 }
             </div>
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import Directory from '../../components/directory/directory.component';
 import './homepage.styles.scss';
 
-const HomePage = () => (
+const HomePage = () => 
+(
     <div className='homepage'>
         <Directory></Directory>
     </div>
